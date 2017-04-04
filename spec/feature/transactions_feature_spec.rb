b76593c64a5ab3600fc0bde963e805c4facd0a3b
glynester <glynester@hotmail.com>
2017-04-04 15:45:01 +0100

    Added amount and % of total to category summary. Test not passing.

--- spec/feature/transactions_feature_spec.rb
+++ spec/feature/transactions_feature_spec.rb
@@ -18,12 +18,22 @@ feature '#Transactions' do
     before do
       @category = Category.create(name: 'food')
       # @restaurant = @user.restaurants.create(name: 'KFC')
-      @transaction = Transaction.create(date: '10/03/2017', amount: '23.00', vendor: 'Pret', trans_type: '\'debit\'', category_id: 1)
+      Transaction.create(date: '10/03/2017', amount: '23.00', vendor: 'Pret', trans_type: 'debit', category_id: 1)
+      Transaction.create(date: '15/03/2017', amount: '27.00', vendor: 'Orange', trans_type: 'debit', category_id: 1)
+      Transaction.create(date: '19/03/2017', amount: '40.00', vendor: 'Sainsburys', trans_type: 'debit', category_id: 2)
+
     end
 
     scenario 'should display categories for existing transactions' do
       visit transactions_path
       expect(page).to have_content 'food'
+    end
+
+    scenario "should display transaction total for category" do
+      visit transactions_path
+      @transactions = Transaction.all
+      p @transactions
+      expect(page).to have_content '50'
     end
   end
 end

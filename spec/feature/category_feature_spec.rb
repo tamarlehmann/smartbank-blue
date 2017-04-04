a6cdf578d8f68a8af88a080746cd802e56719065
John Ashton <John_ashton@hotmail.com>
2017-04-04 17:12:01 +0100

    Add current target page

--- spec/feature/category_feature_spec.rb
+++ spec/feature/category_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature '#Categories' do
-  context 'No transactions:' do
+  context 'Transactions:' do
     scenario 'should display a target spending creation link' do
       visit categories_path
       expect(page).to have_link 'Create Spending Target'
@@ -13,11 +13,16 @@ feature '#Categories' do
       expect(page).to have_content 'Transaction'
       expect(page).to have_content 'Amount'
     end
-  end
 
-  context 'With transactions:' do
     scenario 'should display a category' do
       visit categories_path
+      expect(page).to have_content 'Food'
+    end
+  end
+
+  xcontext 'Edit category:' do
+    scenario 'should allow you to set a category target' do
+      visit edit_category_path(1)
       expect(page).to have_content 'Food'
     end
   end

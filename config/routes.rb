9dca52552a4d169b78d7e75dcfd283a747123ca4
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-04-04 13:10:59 +0100

    Create category index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   root "transactions#index"
 
-  resources :transactions
+  resources :transactions, :categories
 
 end
---
+++ spec/feature/category_feature_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature '#Categories' do
+  context 'No transactions:' do
+      scenario 'should display a no transactions message' do
+        visit categories_path
+        expect(page).to have_content 'You have no transactions to show for this category'
+      end
+      scenario 'should display table headings' do
+        visit transactions_path
+        expect(page).to have_content 'Date'
+        expect(page).to have_content 'Transaction'
+        expect(page).to have_content 'Amount'
+      end
+    end
+end
